0a1c48b1deb76d8453f1dab9d5c26961bfdd69de
tonebula <absolutesdev@gmail.com>
2023-12-05 11:08:12 +0500

    s

--- base.js
+++ base.js
@@ -1,24 +1,2 @@
-import express from 'express'
-import path from 'path'
+gsap.registerPlugin(ScrollTrigger);
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-    
-let app = express();
-
-app.get('',function(req,res){
-   res.sendFile(path.join(__dirname + '/index.html')); 
-});
-
-app.get('/js/main.js',function(req,res){
-    res.sendFile(path.join(__dirname + '/js/main.js')); 
-});
-
-
-
-app.listen(80, function() {
-	console.log('TIME TO CHNG');
-});
