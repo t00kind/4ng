2fd5ff0dfc629679b71141dfd29c13ce0713a45d
tonebula <absolutesdev@gmail.com>
2023-12-09 15:55:20 +0500

    now

--- base.js
+++ base.js
@@ -1,2 +1,60 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(Observer) 
 
+const tl = gsap.timeline(); 
+
+ScrollTrigger.defaults({
+  scroller: ".main"
+});
+
+tl.to(".centered", {
+  scrollTrigger: {
+    trigger: ".centered",
+    start: () => `+= ${window.innerHeight}`,
+    scrub: true,
+    // toggleActions: "restart none reverse none"
+    },
+  top: "5%",
+  fontSize: "5vh",
+  duration: 3,
+  // fontFamily: 'Jetbrains Mono'
+
+  })
+  .to("#main", {
+  scrollTrigger: {
+    trigger: "#main",
+    toggleActions: "restart none none none reverse",
+    scrub: 3
+  },
+  opacity: 1,
+  duration: 7,
+})
+.to('#create', {
+  scrollTrigger: {
+    trigger: "#create",
+    start: "center center",
+    onUpdate: ({ progress}) => {console.log(progress)},
+    pin: true,
+    markers: true,
+    end: "+=1700"
+  },
+  
+})
+  
+
+// Observer.create({
+//   target: window, // can be any element (selector text is fine)
+//   type: "wheel,touch", // comma-delimited list of what to listen for
+//   ignore: "#main",
+//   onPress: () => {alert("DONT TOUCH IT!!")}
+// });
+
+// gsap.to("#co", {
+//   scrollTrigger: {
+//     trigger: "#co",
+//   },
+//   duration: 3,
+//   scrub: true,
+//   onLeave: () => {document.body.style="background: white; color: black"}
+
+// });
