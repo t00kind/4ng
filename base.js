9ec791667c70410861fcdf1dc5ac342d55268094
tonebula <absolutesdev@gmail.com>
2023-12-09 23:02:01 +0500

    all in your hand

--- base.js
+++ base.js
@@ -1,7 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Observer) 
 
-const tl = gsap.timeline(); 
+const tl = gsap.timeline();
+let textElement = document.getElementById("w")
 
 ScrollTrigger.defaults({
   scroller: ".main"
@@ -33,13 +34,40 @@ tl.to(".centered", {
   scrollTrigger: {
     trigger: "#create",
     start: "center center",
-    onUpdate: ({ progress}) => {console.log(progress)},
+    onUpdate: ({ progress}) => {prg = progress},
     pin: true,
     markers: true,
-    end: "+=1700"
+    end: () => `+=${document.getElementById("main").offsetHeight * 10}`,
+    onUpdate: self => {
+      // Получаем прогресс прокрутки
+      const progress = self.progress.toFixed(2);
+  
+      // Здесь вы можете использовать прогресс для изменения текста с анимацией
+      if (progress < 0.3) {
+        gsap.to(textElement, { 
+          textContent: "ВСЕ В ТВОИХ РУКАХ", 
+          ease: "power3.out", 
+          duration: 0.5 
+        });
+      } else if (progress > 0.3 && progress < 0.6) {
+        gsap.to(textElement, { 
+          textContent: "ЛОМАЙ РАМКИ", 
+          ease: "power3.out", 
+          duration: 0.5 
+        })}else {
+          gsap.to(textElement, { 
+            textContent: "МЕНЯЙ МИР", 
+            ease: "power3.out", 
+            duration: 0.5 
+
+        }
+
+        );
+      }
+    }
   },
   
-})
+});
   
 
 // Observer.create({
