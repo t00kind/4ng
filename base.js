b5892779aed264982cf22213621fdbf3292337e0
tonebula <absolutesdev@gmail.com>
2023-12-11 17:32:18 +0500

    now

--- base.js
+++ base.js
@@ -1,26 +1,69 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(Observer) 
-
-const tl = gsap.timeline();
-let textElement = document.getElementById("w")
-
+gsap.registerPlugin(Observer);
 ScrollTrigger.defaults({
   scroller: ".main"
 });
 
-tl.to(".centered", {
-  scrollTrigger: {
-    trigger: ".centered",
-    start: () => `+= ${window.innerHeight}`,
-    scrub: true,
-    // toggleActions: "restart none reverse none"
-    },
-  top: "5%",
-  fontSize: "5vh",
-  duration: 3,
-  // fontFamily: 'Jetbrains Mono'
+const tl = gsap.timeline();
+let textElement = document.getElementById("w")
+let topg = "change the "
+let mm = gsap.matchMedia()
 
-  })
+mm.add("(max-width: 500px)", () => {
+
+  topg = "change the "
+  tl.to("#logo", {
+    scrollTrigger: {
+      trigger: "#logo",
+      start: () => `+= ${window.innerHeight}`,
+      scrub: true,
+      
+      // toggleActions: "restart none reverse none"
+      },
+    fontSize: "4vh"
+    })
+    gsap.to("#topG", {
+      scrollTrigger: {
+        trigger: "#topG",
+        start: () => `+= ${window.innerHeight}`,
+        scrub: true,
+        
+        // toggleActions: "restart none reverse none"
+        },
+      top: "2.88%",
+      // fontFamily: 'Jetbrains Mono'
+    
+      })
+});
+mm.add("(min-width: 501px)", () => {
+
+  topg = "change the "
+  tl.to("#logo", {
+    scrollTrigger: {
+      trigger: "#logo",
+      start: () => `+= ${window.innerHeight}`,
+      scrub: true,
+      
+      // toggleActions: "restart none reverse none"
+      },
+    fontSize: "5vh"
+    })
+    gsap.to("#topG", {
+      scrollTrigger: {
+        trigger: "#topG",
+        start: () => `+= ${window.innerHeight}`,
+        scrub: true,
+        
+        // toggleActions: "restart none reverse none"
+        },
+      top: "3.6%",
+      // fontFamily: 'Jetbrains Mono'
+    
+      })
+});
+  
+
+tl
   .to("#main", {
   scrollTrigger: {
     trigger: "#main",
@@ -34,40 +77,83 @@ tl.to(".centered", {
   scrollTrigger: {
     trigger: "#create",
     start: "center center",
-    onUpdate: ({ progress}) => {prg = progress},
     pin: true,
-    markers: true,
-    end: () => `+=${document.getElementById("main").offsetHeight * 10}`,
+    scrub: true,
+    end: () => `+=${document.getElementById("main").offsetHeight * 30}`,
+    onToggle: (self) => (f = self.isActive),
+    onUpdate: self => {
+      // Получаем прогресс прокрутки
+      const progress = self.progress.toFixed(2);
+  
+      if (progress < 0.3) {
+        // tl.to("#what", {
+        //   textContent: "yourself",
+        // }).fromTo(textElement, {opacity: 0}, { 
+        //   textContent: "ВСЕ В ТВОИХ РУКАХ", 
+        //   opacity: 1
+        // });
+        document.getElementById("what").textContent = "yourself";
+        document.getElementById("w").textContent = "ВСЕ В ТВОИХ РУКАХ";
+        document.getElementById("log").textContent = "change ";
+      } else if (progress > 0.3 && progress < 0.6) {
+      
+        document.getElementById("what").textContent = "your life"
+        document.getElementById("w").textContent = "ЛОМАЙ РАМКИ"
+        document.getElementById("log").textContent = "change "
+      }else if (progress > 0.6){
+          document.getElementById("what").textContent = "world"
+          document.getElementById("log").textContent = topg
+          document.getElementById("w").textContent = "МЕНЯЙ МИР"
+
+
+      }
+  },
+  onLeave: () => {
+    gsap.fromTo("#logo", {textContent: "change"}, {
+      opacity: 1,
+      textContent: "changing",
+      fontFamily: 'Jetbrains Mono',
+      scrollTrigger: {
+        trigger: "#trgg",
+        scrub: true
+      },
+    })
+    gsap.to(".main", {
+      background: "red",
+      duration: 2
+    });
+  }
+  }
+}).to("#now",{
+  scrollTrigger: {
+    trigger: "#now", 
+    start: "center center",
+    pin: true,
     onUpdate: self => {
       // Получаем прогресс прокрутки
       const progress = self.progress.toFixed(2);
   
       // Здесь вы можете использовать прогресс для изменения текста с анимацией
-      if (progress < 0.3) {
-        gsap.to(textElement, { 
-          textContent: "ВСЕ В ТВОИХ РУКАХ", 
+      if (progress > 0.5 && !an) {
+        gsap.fromTo("#now", {opacity: 0},{ 
+          textContent: "ТО ТОЛЬКО СЕЙЧАС", 
           ease: "power3.out", 
-          duration: 0.5 
+          duration: 2,
+          opacity: 1
+          
         });
-      } else if (progress > 0.3 && progress < 0.6) {
-        gsap.to(textElement, { 
-          textContent: "ЛОМАЙ РАМКИ", 
+        an = true
+      } else if (progress < 0.5){
+        gsap.to("#now", { 
+          textContent: "И ЕСЛИ УЖ МЕНЯТЬ", 
           ease: "power3.out", 
-          duration: 0.5 
-        })}else {
-          gsap.to(textElement, { 
-            textContent: "МЕНЯЙ МИР", 
-            ease: "power3.out", 
-            duration: 0.5 
-
-        }
-
-        );
+          duration: 2,
+        });
+        an = false;
       }
-    }
-  },
-  
-});
+    },
+    end:  () => `+=${document.getElementById("main").offsetHeight * 10}`}});
+
   
 
 // Observer.create({
